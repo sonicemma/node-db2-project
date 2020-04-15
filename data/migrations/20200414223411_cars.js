d43997c0bea6008cface2690b7b35b6db3707994
emma <sonicjane2020@gmail.com>
2020-04-14 22:59:48 -0700

    Created table

---
+++ data/migrations/20200414223411_cars.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+      tbl.text('make');
+      tbl.text('model');
+      tbl.integer('mileage');
+      tbl.integer('VIN')
+        .unique()
+        .notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
+};
